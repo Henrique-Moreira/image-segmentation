db0f9b5bc031d3684df432685d5de35eca1a4a54
Jean Ferreira <jeanalves.ferreira@ufu.br>
2024-11-14 00:04:28 -0300

    Renomeia notebook de experimento para a pasta de anotações e atualiza conteúdo, removendo questões e adicionando seção sobre análise de imagens anotadas com Labelme.

---
+++ experimentos/segmentacao/experimento-u-net.py
@@ -0,0 +1,350 @@
+import os
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import json
+import cv2
+import torch
+import os.path as osp
+import glob
+import torchvision
+import matplotlib.pyplot as plt
+
+# Definição da classe UNetVgg
+class UNetVgg(torch.nn.Module):
+    """
+    BorderNetwork is a NN that aims to detected border and classify occlusion.
+    The architecture is a VGG without the last pool layer. After that we 
+    have two paths, one for regression and one for classification (occlusion).
+    """
+    
+    def __init__(self, nClasses):
+        super(UNetVgg, self).__init__()
+        
+        vgg16pre = torchvision.models.vgg16(pretrained=True)
+        self.vgg0 = torch.nn.Sequential(*list(vgg16pre.features.children())[:4])
+        self.vgg1 = torch.nn.Sequential(*list(vgg16pre.features.children())[4:9])
+        self.vgg2 = torch.nn.Sequential(*list(vgg16pre.features.children())[9:16])
+        self.vgg3 = torch.nn.Sequential(*list(vgg16pre.features.children())[16:23])
+        self.vgg4 = torch.nn.Sequential(*list(vgg16pre.features.children())[23:30])
+        
+        
+        self.smooth0 = torch.nn.Sequential(
+                torch.nn.Conv2d(128, 64, kernel_size=(3,3), stride=1, padding=(1, 1)),
+                torch.nn.ReLU(True),
+                torch.nn.Conv2d(64, 64, kernel_size=(3,3), stride=1, padding=(1, 1)),
+                torch.nn.ReLU(True)
+                )
+        self.smooth1 = torch.nn.Sequential(
+                torch.nn.Conv2d(256, 64, kernel_size=(3,3), stride=1, padding=(1, 1)),
+                torch.nn.ReLU(True),
+                torch.nn.Conv2d(64, 64, kernel_size=(3,3), stride=1, padding=(1, 1)),
+                torch.nn.ReLU(True)
+                )
+        self.smooth2 = torch.nn.Sequential(
+                torch.nn.Conv2d(512, 128, kernel_size=(3,3), stride=1, padding=(1, 1)),
+                torch.nn.ReLU(True),
+                torch.nn.Conv2d(128, 128, kernel_size=(3,3), stride=1, padding=(1, 1)),
+                torch.nn.ReLU(True)
+                )
+        self.smooth3 = torch.nn.Sequential(
+                torch.nn.Conv2d(1024, 256, kernel_size=(3,3), stride=1, padding=(1, 1)),
+                torch.nn.ReLU(True),
+                torch.nn.Conv2d(256, 256, kernel_size=(3,3), stride=1, padding=(1, 1)),
+                torch.nn.ReLU(True)
+                )
+        
+        
+        self.final = torch.nn.Conv2d(64, nClasses, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        """
+        Args:
+            x (torch.tensor): A tensor of size (batch, 3, H, W)
+        Returns:
+            reg_out (torch.tensor): A tensor with results of the regression (batch, 4).
+            cls_out (torch.tensor): A tensor with results of the classification (batch, 2).
+        """
+        
+        feat0 = self.vgg0(x)
+        feat1 = self.vgg1(feat0)
+        feat2 = self.vgg2(feat1)
+        feat3 = self.vgg3(feat2)
+        feat4 = self.vgg4(feat3)
+        
+        _,_,H,W = feat3.size()
+        up3 = torch.nn.functional.interpolate(feat4, size=(H,W), mode='bilinear', align_corners=False)
+        concat3 = torch.cat([feat3, up3], 1)
+        end3 = self.smooth3(concat3)
+        
+        _,_,H,W = feat2.size()
+        up2 = torch.nn.functional.interpolate(end3, size=(H,W), mode='bilinear', align_corners=False)
+        concat2 = torch.cat([feat2, up2], 1)
+        end2 = self.smooth2(concat2)
+        
+        _,_,H,W = feat1.size()
+        up1 = torch.nn.functional.interpolate(end2, size=(H,W), mode='bilinear', align_corners=False)
+        concat1 = torch.cat([feat1, up1], 1)
+        end1 = self.smooth1(concat1)
+        
+        _,_,H,W = feat0.size()
+        up0 = torch.nn.functional.interpolate(end1, size=(H,W), mode='bilinear', align_corners=False)
+        concat0 = torch.cat([feat0, up0], 1)
+        end0 = self.smooth0(concat0)
+        
+        return self.final(end0)
+    
+    
+    @staticmethod
+    def eval_net_with_loss(model, inp, gt, class_weights, device):
+        """
+        Evaluate network including loss.
+        
+        Args:
+            model (torch.nn.Module): The model.
+            inp (torch.tensor): A tensor (float32) of size (batch, 3, H, W)
+            gt (torch.tensor): A tensor (long) of size (batch, 1, H, W) with the groud truth (0 to num_classes-1).
+            class_weights (list of float): A list with len == num_classes.
+            device (torch.device): device to perform computation
+            
+        Returns:
+            out (torch.tensor): Network output.
+            loss (torch.tensor): Tensor with the total loss.
+                
+        """
+        weights = torch.from_numpy(np.array(class_weights, dtype=np.float32)).to(device)
+        out = model(inp)
+        
+        softmax = torch.nn.functional.log_softmax(out, dim = 1)
+        loss = torch.nn.functional.nll_loss(softmax, gt, ignore_index=-1, weight=weights)
+            
+        return (out, loss)
+    
+    @staticmethod
+    def get_params_by_kind(model, n_base = 7):
+    
+        base_vgg_bias = []
+        base_vgg_weight = []
+        core_weight = []
+        core_bias = []
+    
+        for name, param in model.named_parameters():
+            if 'vgg' in name and ('weight' in name or 'bias' in name):
+                vgglayer = int(name.split('.')[-2])
+                
+                if vgglayer <= n_base:
+                    if 'bias' in name:
+                        print('Adding %s to base vgg bias.' % (name))
+                        base_vgg_bias.append(param)
+                    else:
+                        base_vgg_weight.append(param)
+                        print('Adding %s to base vgg weight.' % (name))
+                else:
+                    if 'bias' in name:
+                        print('Adding %s to core bias.' % (name))
+                        core_bias.append(param)
+                    else:
+                        print('Adding %s to core weight.' % (name))
+                        core_weight.append(param)
+                        
+            elif ('weight' in name or 'bias' in name):
+                if 'bias' in name:
+                    print('Adding %s to core bias.' % (name))
+                    core_bias.append(param)
+                else:
+                    print('Adding %s to core weight.' % (name))
+                    core_weight.append(param)
+                    
+        return (base_vgg_weight, base_vgg_bias, core_weight, core_bias)
+    
+# End class
+
+# Configuração do dispositivo CUDA
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+cuda_available = torch.cuda.is_available()
+print(f'CUDA disponível: {cuda_available}')
+
+if cuda_available:
+    gpu_name = torch.cuda.get_device_name(0)
+    vram_total = torch.cuda.get_device_properties(0).total_memory / (1024 ** 3)  # Convertendo para GB
+    vram_available = torch.cuda.memory_reserved(0) / (1024 ** 3)  # Convertendo para GB
+    print(f'Nome da GPU: {gpu_name}')
+    print(f'VRAM Total: {vram_total:.2f} GB')
+
+# Caminho do diretório Dataset
+directory = "C:/git/image-segmentation/Dataset/mamoeiro"
+print(f'Diretório do Projeto {directory}.')
+if not os.path.exists(directory):
+    os.makedirs(directory)
+img_folder_val = directory + r'\Val'
+img_folder_train = directory + r'\Train'
+img_folder_test = directory + r'\Test'
+save_dir = directory + r'\\result_psp\\'
+if not os.path.exists(img_folder_val):
+    os.makedirs(img_folder_val)
+if not os.path.exists(img_folder_train):
+    os.makedirs(img_folder_train)
+if not os.path.exists(img_folder_test):
+    os.makedirs(img_folder_test)
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+# Local onde o Modelo será salvo
+model_file_name = save_dir + 'model_segmentadas_unet.pth'
+
+# Configurações do treinamento
+resolution_input = (640, 480)  # Tamanho de entrada
+patience = 30
+plot_val = True
+plot_train = True
+max_epochs = 100
+class_weights = [1, 1, 1, 1]
+nClasses = 4
+
+# Mapeamento de classes e cores
+class_to_color = {'Doenca': (255, 0, 0), 'Folha': (0, 255, 0), 'Solo': (0, 0, 255), 'Saudavel': (0, 255, 255)}
+class_to_id = {'Doenca': 0, 'Folha': 1, 'Solo': 2, 'Saudavel': 3}
+id_to_class = {v: k for k, v in class_to_id.items()}
+
+# Definição do Dataset
+class SegmentationDataset(Dataset):
+    """Segmentation dataset loader."""
+
+    def __init__(self, json_folder, img_folder, is_train, class_to_id, resolution_input=(640, 480), augmentation=False, transform=None):
+        self.gt_file_list = glob.glob(osp.join(json_folder, '*.json'))
+        self.total_samples = len(self.gt_file_list)
+        self.img_folder = img_folder
+        self.is_train = is_train
+        self.transform = transform
+        self.augmentation = augmentation
+        self.resolution = resolution_input
+        self.class_to_id = class_to_id
+        self.mean = [0.485, 0.456, 0.406]
+        self.std = [0.229, 0.224, 0.225]
+
+    def __len__(self):
+        return self.total_samples
+
+    def __getitem__(self, idx):
+        gt_file = self.gt_file_list[idx]
+        img_number_str = osp.splitext(osp.basename(gt_file))[0]
+        
+        # Verificação de existência de arquivos
+        if not osp.exists(gt_file):
+            raise FileNotFoundError(f"Arquivo JSON não encontrado: {gt_file}")
+        
+        # Extrair o nome da imagem considerando que o nome da classe está no meio do nome do arquivo
+        img_path = osp.join(self.img_folder, img_number_str + '.JPG')
+        
+        if not osp.exists(img_path):
+            raise FileNotFoundError(f"Imagem não encontrada: {img_path}")
+        
+        gt_json = json.load(open(gt_file, 'r'))
+        img_np = cv2.imread(img_path, cv2.IMREAD_IGNORE_ORIENTATION + cv2.IMREAD_COLOR)
+        
+        if img_np is None:
+            raise FileNotFoundError(f"Imagem não encontrada: {img_path}")
+        
+        original_shape = img_np.shape
+        img_np = cv2.resize(img_np, (self.resolution[0], self.resolution[1]))[..., ::-1]
+        img_np = np.ascontiguousarray(img_np)
+        label_np = np.zeros((img_np.shape[0], img_np.shape[1]))
+        label_np[...] = -1
+
+        for shape in gt_json['shapes']:
+            points_np = np.array(shape['points'], dtype=np.float64)
+            points_np[:, 0] *= self.resolution[0] / original_shape[1]
+            points_np[:, 1] *= self.resolution[1] / original_shape[0]
+            points_np = np.round(points_np).astype(np.int64)
+            points_np = points_np.reshape((-1, 1, 2))
+            label = shape['label']
+            if label not in self.class_to_id:
+                raise KeyError(f"Label '{label}' não encontrado em class_to_id")
+            label_np = cv2.fillPoly(label_np, [points_np], self.class_to_id[label])
+
+        label_np = label_np.astype(np.int32)
+
+        if self.is_train and self.augmentation:
+            if np.random.rand() > 0.5:
+                img_np = np.fliplr(img_np)
+                label_np = np.fliplr(label_np)
+                img_np = np.ascontiguousarray(img_np)
+                label_np = np.ascontiguousarray(label_np)
+
+        img_pt = img_np.astype(np.float32) / 255.0
+        for i in range(3):
+            img_pt[..., i] -= self.mean[i]
+            img_pt[..., i] /= self.std[i]
+
+        img_pt = img_pt.transpose(2, 0, 1)
+        img_pt = torch.from_numpy(img_pt)
+        label_pt = torch.from_numpy(label_np).long()
+
+        sample = {'image': img_pt, 'gt': label_pt, 'image_original': img_np}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+# Inicializar listas para armazenar a perda e a precisão
+train_losses = []
+train_accuracies = []
+val_accuracies = []
+
+# Inicia o treinamento
+train_dataset = SegmentationDataset(img_folder_train, img_folder_train, True, class_to_id, resolution_input, True)
+print(f"Número de amostras no dataset de treinamento: {len(train_dataset)}")
+print(f"Arquivos no dataset de treinamento: {os.listdir(img_folder_train)}")
+
+val_dataset = SegmentationDataset(img_folder_val, img_folder_val, False, class_to_id, resolution_input)
+print(f"Número de amostras no dataset de validação: {len(val_dataset)}")
+print(f"Arquivos no dataset de validação: {os.listdir(img_folder_val)}")
+
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=0, drop_last=True)
+val_loader = DataLoader(val_dataset, batch_size=1, shuffle=True, num_workers=0, drop_last=False)
+
+if plot_train:
+    for i_batch, sample_batched in enumerate(train_loader):
+        image_np = np.squeeze(sample_batched['image_original'].cpu().numpy())
+        gt = np.squeeze(sample_batched['gt'].cpu().numpy())
+        
+        color_label = np.zeros((resolution_input[1], resolution_input[0], 3))
+        
+        for key, val in id_to_class.items():
+            color_label[gt == key] = class_to_color.get(val, [0, 0, 0])  # Provide a default color if key is missing
+        
+        plt.figure()
+        plt.imshow((image_np / 255) * 0.5 + (color_label / 255) * 0.5)
+        plt.show()
+        
+        plt.figure()
+        plt.imshow(color_label.astype(np.uint8))
+        plt.show()
+
+model = UNetVgg(nClasses).to(device)
+
+core_lr = 0.02
+base_vgg_weight, base_vgg_bias, core_weight, core_bias = UNetVgg.get_params_by_kind(model, 7)
+
+optimizer = torch.optim.SGD([{'params': base_vgg_bias, 'lr': 0.000001}, 
+                             {'params': base_vgg_weight, 'lr': 0.000001},
+                             {'params': core_bias, 'lr': core_lr},
+                             {'params': core_weight, 'lr': core_lr, 'weight_decay': 0.0005}], momentum=0.9)
+
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 30, gamma=0.2)
+
+best_val_acc = -1
+best_epoch = 0
+
+# Start training...
+for epoch in range(max_epochs):
+    print('Epoch %d starting...' % (epoch+1))
+    
+    lr_scheduler.step()
+    
+    model.train()
+    
+    mean_loss = 0.0
+    
+    n_correct = 0
+    n_false = 0
